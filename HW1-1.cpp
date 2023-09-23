b1facd84307b47f6e1f25ad3b34b0f6d74641f6e
Jia Chiuan <b1129046@cgu.edu.tw>
2023-09-23 22:17:30 +0800

    homewor p1

---
+++ HW1-1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void Bubblesort(string** arr,int x){
+    for(int i=0;i<x-1;i++){
+        if(arr[i][0]>arr[i+1][0]){
+            string temp = arr[i][0];
+            arr[i][0] = arr[i+1][0];
+            arr[i+1][0] = temp;
+        }
+    }
+}
+
+int main(){
+       int x,y;
+       string input;
+       cout<<"Please input how many arrays and how many characters in each array:"<<endl;
+       cin>>x>>y;
+       string** arr = new string* [x];
+       for(int i=0;i<x;i++){
+            arr[i] = new string[y+1];
+       }
+       for(int i=0;i<x;i++){
+            cout<<"Please enter the "<<i+1<<" array:"<<endl;
+            cin>>input;
+            for(int j=0;j<y;j++){
+                arr[i][j] = input[j];
+            }
+
+       }       
+    Bubblesort(arr,x);
+    cout<<"After sorting:"<<endl;
+    
+    for(int i=0;i<x;i++){
+        for(int j=0;j<y;j++){
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+}
