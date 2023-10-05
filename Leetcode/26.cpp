3e7e95925ef1a2cf34dc50f8f04d891de2fc2586
Jia Chiuan <b1129046@cgu.edu.tw>
2023-10-05 18:37:33 +0800

    26 diy

--- Leetcode/26.cpp
+++ Leetcode/26.cpp
@@ -1,35 +1,27 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int> ans;
-        int count = nums.size(); ;
-        for(int i = 0;i<nums.size()-1;i++){
-            for(int j=i;j<nums.size()-1;j++){
-                if(nums[i]==nums[j+1] && nums[i]!=' '){
-                    nums.erase(nums.begin()+j+1);
+        int count = nums.size();
+        for(int i = 0 ; i< nums.size(); i++){
+            for(int j = i+1;j<nums.size();j++){
+                if(nums[i] == nums[j]){
+                    nums.erase(nums.begin()+j);
+                    j--;
                     count--;
                 }
-            } 
+            }
         }
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i]!=' '){
+        vector<int> ans;
+        for(int i = 0 ; i< nums.size(); i++){
+            if(nums[i] !=' '){
                 ans.push_back(nums[i]);
             }
         }
-        for(int i = count;size.of(ans)>i;i++){
-            ans.push_back(' ');
-        }
-        nums = ans;
-        cout<<count<<", "<<"nums = "<<nums<<endl;
+        return count;
     }
 };
-int main(){
-    solution s;
-
-    removeDuplicates(nums);
-}
---
+++ Leetcode/35.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i = 0;
+        for(; i < nums.size(); i++){
+            if(nums[i] >= target){
+                break;
+            }
+        }
+        return i;
+    }
+};
