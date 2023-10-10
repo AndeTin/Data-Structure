2491afd53f750ee9c32b0496eeabdea35e5e7591
Jia Chiuan <b1129046@cgu.edu.tw>
2023-10-11 02:24:49 +0800

    455

--- HW2-1.cpp
+++ HW2-1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<time.h>
+#include<chrono>
 
 using namespace std;
 
@@ -22,22 +23,21 @@ int BinarySearch(int*a, const int x, const int n){
 }
 
 int main(){
-    for(int n=1; n<=100; n++){
+    for(int n=1; n<=10; n++){
         int *a = new int[n];
         for(int i=0; i<n; i++){
             a[i] = i;
         }
-        //srand(time(NULL));
         int x = rand() % n;
-        auto start = clock();
+        double start = clock();
+        cout <<start<<endl;
         int index = BinarySearch(a, x, n);
-        auto end = clock();
-        double t = static_cast<double>((end - start) /(double)CLOCKS_PER_SEC);
+        double end = clock();
         if(index == -1){
             cout << "NO."<<n<<" Not found" << endl;
         }
         else{
-            cout << "NO." << n << " index= "<< index << " time cost: "<< t << endl;
+            cout << "NO." << n <<'\t'<< "index= "<< index << '\t' << "time cost: "<< (end-start)/CLOCKS_PER_SEC<< endl;
         }
         delete []a;
 
