4ff2b220a21a9c848eec6fcf6b56abcc86e4b7a7
Jia Chiuan <b1129046@cgu.edu.tw>
2023-11-28 00:46:39 +0800

    version 2

--- HomeWork/HW4.cpp
+++ HomeWork/HW4.cpp
@@ -11,55 +11,30 @@ class linklist{
                 friend class linklist;
             public:
                 node(){
-                    double exp;
-                    double coef;
-                    node *next;
+                    exp = 0;
+                    coef = 0;
+                    next = 0;
                 }     
         };
         node *head;
     public:
         linklist(){
-            int size;
+            size = 0;
+            head = 0;
         }
         void insert(){
             cout << "Please input the size of the polynomial:" << endl;
             cin >> size;
-            node *head = new node[size];
+            head = new node[size];
             for(int i = 0; i < size; i++){
-                cout << "Please input the coefficient of the polynomial" << i+1 << ":" << endl;
+                cout << "Please input the coefficient of the polynomial " << i+1 << ":" << endl;
                 cin >> head[i].coef ;
-                cout << endl << "Please input the exponent of the polynomial" << i+1 << ":" << endl;
+                cout << endl << "Please input the exponent of the polynomial " << i+1 << ":" << endl;
                 cin >> head[i].exp;
                 cout << endl;
             }
             for(int i = 0; i < size; i++){
-                head[i].next = &head[i+1];
-            }
-            head[size-1].next = 0;
-        }
-        void print(){
-            node temp = head[0];
-            while(temp.next != 0){
-                cout << temp.coef << "x^" << temp.exp ;
-                temp = *temp.next;
-                if(temp.next != 0){
-                    cout << " + ";
-                }
-            }
-            cout << endl;
-        }
-        void multiply(linklist &a, linklist &b){
-            linklist c ;
-            c.size = a.size * b.size;
-            node *head = new node[c.size];
-            for(int i = 0; i < a.size; i++){
-                for(int j = 0; j < b.size; j++){
-                    head[i * b.size + j].coef = a.head[i].coef * b.head[j].coef;
-                    head[i * b.size + j].exp = a.head[i].exp + b.head[j].exp;
-                }
-            }
-            for(int i = 0; i < c.size; i++){
-                for(int j = i+1; j < c.size; j++){
+                for(int j = i+1; j < size; j++){
                     if(head[i].exp < head[j].exp){
                         node temp = head[i];
                         head[i] = head[j];
@@ -67,12 +42,75 @@ class linklist{
                     }
                 }
             }
-            for(int i = 0; i < c.size; i++){
+            for(int i = 0; i < size; i++){
                 head[i].next = &head[i+1];
             }
-            head[c.size-1].next = 0;
-            c.print();
+            head[size-1].next = 0;
+        }
+        void multiply(linklist &a, linklist &b){
+            linklist c ;
+            c.size = a.size * b.size;
+            c.head = new node[c.size];
+            for(int i = 0; i < a.size; i++){
+                for(int j = 0; j < b.size; j++){
+                    c.head[i*b.size+j].coef = a.head[i].coef * b.head[j].coef;
+                    c.head[i*b.size+j].exp = a.head[i].exp + b.head[j].exp;
+                }
+            }
+            for(int i = 0; i < c.size; i++){
+                for (int j = i+1; j < c.size; j++){
+                    if(c.head[i].exp < c.head[j].exp){
+                        node temp = c.head[i];
+                        c.head[i] = c.head[j];
+                        c.head[j] = temp;
+                    }
+                }
+            }
+            for(int i = 0; i < c.size; i++){
+                if(i == c.size-1)
+                    c.head[i].next = 0;
+                else
+                    c.head[i].next = &c.head[i+1];
+            }
 
+            cout << "The result of the multiplication without simp is:" << endl;
+            for(int i = 0; i < c.size; i++){
+                cout << c.head[i].coef << "x^" << c.head[i].exp;
+                if(i != c.size-1){
+                    cout << " + ";
+                }
+            }
+            cout << endl;
+
+            cout << "Begin to simplify the polynomial:" << endl;
+            int count = 0;
+            for(int i = 0; i < c.size; i++){
+                if(c.head[i].exp == c.head[i+1].exp){
+                    c.head[i].coef += c.head[i+1].coef;
+                    for(int j = i+1; j < c.size-1; j++){
+                        c.head[j] = c.head[j+1];
+                        c.head[j].next = &c.head[j+1];
+                    }
+                    c.head[c.size-1].next = 0;
+                    c.head[c.size-1].coef = 0;
+                    c.head[c.size-1].exp = 0;
+                    count++;
+                    cout << count << " times of simplification" << endl;
+                    
+                }
+            }
+
+            cout << "The result of the multiplication is:" << endl;
+            for(int i = 0; i < c.size; i++){
+                if(c.head[i].coef != 0 && c.head[i].exp != 0){
+                    cout << c.head[i].coef << "x^" << c.head[i].exp;
+                }
+                if(i != c.size-1 && c.head[i+1].coef != 0 && c.head[i+1].exp != 0){
+                    cout << " + ";
+                }
+            }
+            cout << endl;
+            free(&c);
         }
 };
 
@@ -82,5 +120,7 @@ int main(){
     linklist b;
     b.insert();
     a.multiply(a, b);
+    free(&a);
+    free(&b);
     return 0;    
 }
