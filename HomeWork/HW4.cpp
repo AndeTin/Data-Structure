64bd825c5af828d39489901acef5068df1fb54ff
Jia Chiuan <b1129046@cgu.edu.tw>
2023-11-28 14:47:17 +0800

    version final

--- HomeWork/HW4.cpp
+++ HomeWork/HW4.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<ctime>
+#include<chrono>
 using namespace std;
 
 class linklist{
@@ -24,27 +26,27 @@ class linklist{
         }
         //set the polynomial
         void insert(){
-            cout << "Please input the size of the polynomial:" << endl;
+            cout << "Please input the size of the polynomial:\n";
             cin >> size;
             head = new node[size];
             for(int i = 0; i < size; i++){
-                cout << "Please input the coefficient of the polynomial " << i+1 << ":" << endl;
+                cout << "Please input the coefficient of the polynomial " << i+1 << ":\n";
                 cin >> head[i].coef >> head[i].exp;
                 // cin >> head[i].exp;
                 while(cin.fail()){
                     cin.clear();
                     cin.ignore(1000, '\n');
-                    cout << "Please enter a number for the coefficient of polynomial " << i+1 << ":" << endl;
+                    cout << "Please enter a number for the coefficient of polynomial " << i+1 << ":\n";
                     cin >> head[i].coef ;
                 }
-                // cout << endl << "Please input the exponent of the polynomial " << i+1 << ":" << endl;
+                // cout << "\n" << "Please input the exponent of the polynomial " << i+1 << ":\n";
                 while(cin.fail()){
                     cin.clear();
                     cin.ignore(1000, '\n');
-                    cout << "Please enter a number for the exponent of polynomial " << i+1 << ":" << endl;
+                    cout << "Please enter a number for the exponent of polynomial " << i+1 << ":\n";
                     cin >> head[i].exp;
                 }
-                cout << endl;
+                cout << "\n";
             }
             for(int i = 0; i < size; i++){
                 for(int j = i+1; j < size; j++){
@@ -71,8 +73,10 @@ class linklist{
                     c.head[i*b.size+j].exp = a.head[i].exp + b.head[j].exp;
                 }
             }
-            cout<< "Calculate finished" << endl;
+            cout<< "Calculate finished\n";
+            
             //sort the polynomial
+            cout << "Sorting\n";
             for(int i = 0; i<c.size; i++){
                 for (int j = i+1; j < c.size; j++){
                     if(c.head[i].exp < c.head[j].exp){
@@ -82,7 +86,20 @@ class linklist{
                     }
                 }
             }
-            cout << "Sort finished" << endl;
+            //simplify the polynomial
+            cout << "Begin to simplify the polynomial:\n";
+            for(int i = 0; i < c.size; i++){
+                for(int j = i+1; j < c.size; j++){
+                    if(c.head[i].exp != c.head[j].exp){
+                        break;
+                    }
+                    else{
+                        c.head[i].coef += c.head[j].coef;
+                        c.head[j].coef = 0;
+                        c.head[j].exp = 0;
+                    }
+                }
+            }
             //set the next pointer
             for(int i = 0; i < c.size; i++){
                 if(i == c.size-1)
@@ -90,25 +107,9 @@ class linklist{
                 else
                     c.head[i].next = &c.head[i+1];
             }
-            cout << "Set next pointer finished" << endl;
-            //simplify the polynomial
-            cout << "Begin to simplify the polynomial:" << endl;
-            int count = 0;
-            node *test = c.head;
-            for(int i = 0 ; i < c.size; i++){
-                if(test->exp == c.head[i+1].exp){
-                    test->coef += c.head[i+1].coef;
-                    test->next = c.head[i+1].next;
-                    c.head[i+1].coef = 0;
-                    count++;
-                }
-                else{
-                    test = test->next;
-                }
-
-            }
+            cout << "Set the next pointer finished\n";
             //print the result
-            cout << "The result of the multiplication is:" << endl;
+            cout << "The result of the multiplication is:\n";
             for(int i = 0; i < c.size; i++){
                 if(c.head[i].coef != 0 && c.head[i].exp != 0){
                     cout << c.head[i].coef << "x^" << c.head[i].exp;
@@ -117,18 +118,22 @@ class linklist{
                     cout << " + ";
                 }
             }
-            cout << endl;
+            cout << "\n";
             //free the memory
             free(&c);
         }
 };
 
 int main(){
+    time_t start, end;
     linklist a;
     a.insert();
     linklist b;
     b.insert();
+    start = chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1);
     a.multiply(a, b);
+    end = chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1);
+    cout << "The time of the program is: " << end - start << "ms\n";
     free(&a);
     free(&b);
     return 0;    
